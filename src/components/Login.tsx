535c5de9d983795b43a5d398d007553e9ea48b8e
X9 <david.agu@iisysgroup.com>
2024-08-20 09:53:27 +0100

    fix: :art: handle submit patch

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,13 +8,28 @@ export default function Login(){
         password: ""
     });
 
-    const handleSubmit = (e) => {
+    const handleSubmitEvent = (e) => {
         e.preventDefault();
         // TODO: add authentication logic here
         console.log(input);
+        if(input.username !== "" && input.password !== ""){
+            // dispatch action from hooks
+        }
+        alert("please provide a valid input");
+    }
+
+    const handleInput = (e) => {
+        const {name, value} = e.target;
+        setInput((prev) => (
+            {
+                ...prev,
+                [name]: value
+            }
+        ))
     }
     return (
         <form>
+            hello there
         </form>
     )
 }
